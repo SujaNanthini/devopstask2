d02bebe70769b5f09fe8a5d738d613831a4a6463
SujaNanthini <sujananthini21@gmail.com>
2024-01-06 22:14:37 +0530

    success

--- src/main/java/com/devopstask2/controller/DevopsTask2Controller.java
+++ src/main/java/com/devopstask2/controller/DevopsTask2Controller.java
@@ -3,13 +3,12 @@ package com.devopstask2.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-public class DevopsTask2Controller {
-	@RestController
-	public class DevopsTaskController {
-		@GetMapping(path = "/getMsg")
-		public String getMsg() {
-			return "Hello sujananthini";
-		}
+@RestController
 
+public class DevopsTask2Controller {
+	@GetMapping(path = "/getMsg")
+	public String getMsg() {
+		return "Hello sujananthini";
 	}
+
 }
